b82e30cfde9fb3e133e663d332c93f9d1b075e40
Celio <celio1878@hotmail.com>
2024-05-07 16:09:45 -0300

    feat: Search Input

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -1,12 +1,25 @@
 import { FC } from "react";
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export const SearchInput: FC = () => {
+  const router = useRouter();
+
   return (
     <div className="flex flex-row w-8/12 items-center">
       <Search className="relative w-4 h-4 left-7 text-slate-400" />
-      <Input className="pl-10" type="search" placeholder="Pesquisar" />
+      <Input
+        className="pl-10"
+        name="search"
+        type="search"
+        placeholder="Pesquisar"
+        onKeyDown={(event) =>
+          event.key === "Enter" &&
+          (event.preventDefault(),
+          router.push(`/search?text=${event.currentTarget.value}&page=1`))
+        }
+      />
     </div>
   );
 };
