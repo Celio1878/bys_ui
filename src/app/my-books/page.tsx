435afd0d81f59eaec528a2fd7878d40c154fbe10
Celio <celio1878@hotmail.com>
2024-05-07 14:01:56 -0300

    feat: Add Suspense component and Update my-books page

--- src/app/my-books/page.tsx
+++ src/app/my-books/page.tsx
@@ -1,26 +1,27 @@
 "use client";
 
-import { PlusIcon } from "lucide-react";
+import { FilePenLine, PlusIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { Suspense } from "react";
 
 export default function MyBooksPage() {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <div className="flex flex-row items-center pt-10 pb-6">
         <h1 className="w-full text-4xl font-bold text-center">Meus Livros</h1>
-        <Button className="absolute right-32 gap-2 items-center justify-center">
+        <Button className="absolute right-5 sm:right-32 sm:gap-2 items-center justify-center">
           <PlusIcon />
-          Novo Livro
+          <p className="hidden sm:block">Novo Livro</p>
         </Button>
       </div>
 
       <div className="flex flex-wrap w-full items-center justify-center">
         {Array.from({ length: 10 }).map((_, i) => (
-          <div key={i} className="p-4">
+          <div key={i} className="flex flex-col w-[10rem] gap-4 p-4">
             <Image
               className={
-                "cursor-pointer hover:scale-110 transition duration-700"
+                "cursor-pointer hover:scale-105 transition duration-700"
               }
               src={"/cover.jpg"}
               alt={"cover"}
@@ -28,9 +29,18 @@ export default function MyBooksPage() {
               height={160}
               priority={true}
             />
+
+            <h3>Titulo</h3>
+            <Button
+              variant="secondary"
+              className="flex flex-row justify-center items-center gap-2"
+            >
+              <FilePenLine />
+              <p>Editar</p>
+            </Button>
           </div>
         ))}
       </div>
-    </>
+    </Suspense>
   );
 }
