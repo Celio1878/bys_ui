78e4f2f02aa500a7b2af60cae558ec4f970fa674
Celio <celio1878@hotmail.com>
2024-05-14 09:38:02 -0300

    feat: Carousel hook

---
+++ src/lib/use-carousel-component.ts
@@ -0,0 +1,67 @@
+import { useCallback, useEffect, useState } from "react";
+import type { CarouselApi } from "@/components/ui/carousel";
+
+interface Props {
+  api: CarouselApi;
+  setApi: (api: CarouselApi) => void;
+  current: number;
+  count: number;
+  books_breakpoints: { [key: string]: { slidesToScroll: number } };
+}
+
+export function useCarouselComponent(): Props {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const handleSelect = useCallback(() => {
+    setCurrent((api?.selectedScrollSnap() as any) + 1);
+  }, [api]);
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    const scrollSnapList = api.scrollSnapList();
+    const newCount = scrollSnapList.length;
+    const newCurrent = api.selectedScrollSnap() + 1;
+
+    if (newCount !== count) {
+      setCount(newCount);
+    }
+
+    if (newCurrent !== current) {
+      setCurrent(newCurrent);
+    }
+
+    api.on("select", handleSelect);
+
+    return () => {
+      api.off("select", handleSelect);
+    };
+  }, [api, current, count, handleSelect]);
+
+  const books_breakpoints = {
+    "(max-width: 640px)": {
+      slidesToScroll: 2,
+    },
+    "(min-width: 768px)": {
+      slidesToScroll: 4,
+    },
+    "(min-width: 1024px)": {
+      slidesToScroll: 5,
+    },
+    "(min-width: 1280px)": {
+      slidesToScroll: 6,
+    },
+  };
+
+  return {
+    api,
+    setApi,
+    current,
+    count,
+    books_breakpoints,
+  };
+}
