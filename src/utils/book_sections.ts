356e6f2d9112f5b5b7c685d765b5a73f2396f260
Celio <celio1878@hotmail.com>
2024-05-14 09:38:22 -0300

    feat: Book sections

---
+++ src/utils/book_sections.ts
@@ -0,0 +1,20 @@
+export const book_sections = [
+  {
+    title: "Original",
+  },
+  {
+    title: "Destaque",
+  },
+  {
+    title: "Terror",
+  },
+  {
+    title: "Fantasia",
+  },
+  {
+    title: "Suspense",
+  },
+  {
+    title: "Romance",
+  },
+];
