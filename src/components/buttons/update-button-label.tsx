594272c65d89d13942bf24f5c7c70d44417b99d1
Celio <celio1878@hotmail.com>
2024-06-07 19:27:27 -0300

    feat: Componentization

---
+++ src/components/buttons/update-button-label.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import { FilePenLine } from "lucide-react";
+
+export const UpdateButtonLabel: FC = () => {
+  return (
+    <>
+      <FilePenLine />
+      <span>Editar</span>
+    </>
+  );
+};
