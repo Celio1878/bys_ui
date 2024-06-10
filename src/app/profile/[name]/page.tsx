0811d84637cf10a043a3f2365bb98591823c674b
Celio <celio1878@hotmail.com>
2024-06-10 11:32:19 -0300

    feat: Create components about user profile

--- src/app/profile/[name]/page.tsx
+++ src/app/profile/[name]/page.tsx
@@ -7,17 +7,33 @@ import { Book } from "@/components/book";
 import { Card } from "@/components/ui/card";
 import { BookDrawer } from "@/components/book-drawer";
 import { UpdateButtonLabel } from "@/components/buttons/update-button-label";
+import { usePathname } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { UserImage } from "@/components/user-image";
+import { FollowComponent } from "@/components/follow-component";
 
-export default function MyBooksPage() {
+export default function ProfilePage() {
+  const { data: session } = useSession() as any;
+  const pathname = usePathname();
+
   return (
     <Suspense fallback={<Loading />}>
+      <section
+        className="flex flex-col items-center gap-4 pb-20"
+        title={"Perfil"}
+      >
+        <UserImage width={150} height={150} />
+        <h1 className="text-2xl font-bold">{session?.user?.name}</h1>
+        <FollowComponent />
+      </section>
+
       <MyBooksHeader />
 
       <Card className="flex flex-wrap w-full items-center justify-center gap-8 py-8">
         {Array.from({ length: 10 }).map((_, i) => {
           const title = "Livro " + i;
           const id = title.replace(/\s/g, "-").toLowerCase();
-          const href = `/my-books/book/${id}`;
+          const href = `${pathname}/book/${id}`;
 
           return (
             <Book
--- src/components/popover_inf_user.tsx
+++ src/components/popover_inf_user.tsx
@@ -5,17 +5,13 @@ import { PopoverUserDataButtons } from "@/components/popover-user-data-buttons";
 
 interface PopoverInfUserProps {
   session?: { picture: string; name: string; email: string };
-  on_close_popover: VoidFunction;
 }
 
-export const PopoverInfUser: FC<PopoverInfUserProps> = ({
-  session,
-  on_close_popover,
-}) => {
+export const PopoverInfUser: FC<PopoverInfUserProps> = ({ session }) => {
   return (
     <PopoverContent className="flex flex-col items-center justify-center divide-y gap-4">
       <PopoverUserData {...{ session }} />
-      <PopoverUserDataButtons {...{ on_close_popover }} />
+      <PopoverUserDataButtons user_name={session?.name as string} />
     </PopoverContent>
   );
 };
