691e8efe1f8ccf10e94829dfde0de1f6934704fa
Celio <celio1878@hotmail.com>
2024-06-22 10:04:08 -0300

    feat: Memoization components

--- src/components/buttons/google-login-button.tsx
+++ src/components/buttons/google-login-button.tsx
@@ -1,12 +1,14 @@
-import { FC } from "react";
+import { FC, memo } from "react";
 import { signIn } from "next-auth/react";
 import { GoogleIcon } from "@/components/icons/google-icon";
 import { Button } from "@/components/ui/button";
 
-export const GoogleLoginButton: FC = () => {
+export const GoogleLoginButton: FC = memo(() => {
   return (
-    <Button className="gap-1" onClick={() => signIn("google")}>
+    <Button className="gap-1.5" onClick={() => signIn("google")}>
       <span>Login</span> <GoogleIcon width={20} height={20} />
     </Button>
   );
-};
+});
+
+GoogleLoginButton.displayName = "GoogleLoginButton";
--- src/components/shell/header.tsx
+++ src/components/shell/header.tsx
@@ -1,15 +1,27 @@
 "use client";
 
-import { FC } from "react";
+import { FC, memo, useEffect } from "react";
 import { ModeToggle } from "@/components/buttons/mode-toggle";
 import { LogoIcon } from "@/components/icons/logo-icon";
 import { useSession } from "next-auth/react";
 import { SearchInput } from "@/components/form/search-input";
 import { PopoverUser } from "@/components/popover-user";
 import { GoogleLoginButton } from "@/components/buttons/google-login-button";
+import { toast } from "@/components/ui/use-toast";
 
-export const Header: FC = () => {
+export const Header: FC = memo(() => {
   const { data: session, status } = useSession() as any;
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      toast({
+        className: "bg-sky-500 text-white",
+        title: `Bem Vindo, ${session?.user.name}!`,
+        description: "Consuma nossos conteudos a vontade.",
+        type: "foreground",
+      });
+    }
+  }, [status]);
 
   return (
     <header className="flex flex-row w-full justify-around items-center gap-4 sm:gap-0 pt-4 px-2">
@@ -25,4 +37,6 @@ export const Header: FC = () => {
       <ModeToggle />
     </header>
   );
-};
+});
+
+Header.displayName = "Header";
