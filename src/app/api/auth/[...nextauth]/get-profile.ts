41d4757f74ff23c1590ff37df83f9b94792e2eb4
Celio <celio1878@hotmail.com>
2024-09-30 05:39:23 -0300

    fix: Env vars

--- src/app/api/auth/[...nextauth]/get-profile.ts
+++ src/app/api/auth/[...nextauth]/get-profile.ts
@@ -2,7 +2,9 @@ import { fetcher } from "@/hooks/fetcher";
 import { ProfileDto } from "@/app/model/profile-dto";
 
 const SERVICE_URL = String(process.env.NEXT_PUBLIC_PROFILES_API_URL);
-const GOOGLE_SIGNIN_ORIGIN = String(process.env.GOOGLE_SIGNIN_ORIGIN);
+const GOOGLE_SIGNIN_ORIGIN = String(
+  process.env.NEXT_PUBLIC_GOOGLE_SIGNIN_ORIGIN,
+);
 
 export async function getProfile(id: string): Promise<ProfileDto> {
   const headers = {
