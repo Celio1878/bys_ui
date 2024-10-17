bc4014d419844ced3bbde84d4023101cd8396291
Celio <celio1878@hotmail.com>
2024-10-17 07:49:52 -0300

    fix: button and image props

--- src/components/buttons/report-button.tsx
+++ src/components/buttons/report-button.tsx
@@ -1,18 +1,23 @@
 import { Flag } from "lucide-react";
-import { FC } from "react";
+import { forwardRef } from "react";
 
 interface ReportButtonProps {
   id: string;
   onClick?: VoidFunction;
 }
 
-export const ReportButton: FC<ReportButtonProps> = ({ id, onClick }) => (
-  <button
-    className="self-center p-2 rounded-full bg-red-100 bg-opacity-50 dark:bg-transparent text-orange-500 hover:text-red-500 hover:bg-red-50 hover:scale-125 transition-all duration-300"
-    name={`report-comment-${id}`}
-    title={`Reportar`}
-    onClick={onClick}
-  >
-    <Flag size={15} />
-  </button>
+export const ReportButton = forwardRef<HTMLButtonElement, ReportButtonProps>(
+  ({ id, onClick }, ref) => (
+    <button
+      className="self-center p-2 rounded-full bg-red-100 bg-opacity-50 dark:bg-transparent text-orange-500 hover:text-red-500 hover:bg-red-50 hover:scale-125 transition-all duration-300"
+      name={`report-comment-${id}`}
+      title={`Reportar`}
+      onClick={onClick}
+      ref={ref}
+    >
+      <Flag size={15} />
+    </button>
+  ),
 );
+
+ReportButton.displayName = "ReportButton";
