96c536fb0c384e6255556fc46b444f57a0ecd980
Celio <celio1878@hotmail.com>
2024-11-04 11:53:57 -0300

    fix: Banner Images imports

---
+++ src/components/books-carousel-section.tsx
@@ -0,0 +1,34 @@
+import { FC } from "react";
+import { BookDto } from "@/app/model/book-dto";
+import { BooksCarousel } from "@/components/carousel/books-carousel";
+import { GenreTags } from "@/app/model/tags";
+
+export const BooksCarouselSection: FC<{ books: BookDto[] }> = ({ books }) => {
+  return (
+    <section className="flex flex-col gap-20 md:w-full">
+      {books?.length! > 0 && (
+        <BooksCarousel
+          key={"highlighted"}
+          sectionTitle={"Lançamentos"}
+          books={books!}
+        />
+      )}
+
+      {GenreTags.map((section) => {
+        const booksByGenre = books?.filter(
+          (book) => book.genre.id === section.id,
+        );
+
+        if (!booksByGenre || booksByGenre.length === 0) return null;
+
+        return (
+          <BooksCarousel
+            key={section.id}
+            sectionTitle={section.title}
+            books={booksByGenre}
+          />
+        );
+      })}
+    </section>
+  );
+};
--- src/utils/banner-images.tsx
+++ src/utils/banner-images.tsx
@@ -4,7 +4,7 @@ export const bannerImages = [
   <picture key={`banner-pic-${1}`}>
     <source
       media="(max-width: 767px)"
-      srcSet={`${BUCKET_URL}/banners/mobile_img1.jpg`}
+      srcSet={`${BUCKET_URL}/banners/desktop_img1.png`}
     />
     <source
       media="(max-width: 1024px)"
@@ -17,14 +17,14 @@ export const bannerImages = [
 
     <img
       src={`${BUCKET_URL}/banners/desktop_img1.png`}
-      className="w-full object-contain md:object-cover rounded-lg"
+      className="w-[40rem] object-contain md:object-cover rounded-lg"
       alt={"banner"}
     />
   </picture>,
   <picture key={`banner-pic-${2}`}>
     <source
       media="(max-width: 767px)"
-      srcSet={`${BUCKET_URL}/banners/mobile_img2.jpg`}
+      srcSet={`${BUCKET_URL}/banners/desktop_img2.png`}
     />
     <source
       media="(max-width: 1024px)"
@@ -37,14 +37,14 @@ export const bannerImages = [
 
     <img
       src={`${BUCKET_URL}/banners/desktop_img2.png`}
-      className="w-full object-contain md:object-cover rounded-lg"
+      className="w-[40rem] object-contain md:object-cover rounded-lg"
       alt={"banner"}
     />
   </picture>,
   <picture key={`banner-pic-${3}`}>
     <source
       media="(max-width: 767px)"
-      srcSet={`${BUCKET_URL}/banners/mobile_img3.jpg`}
+      srcSet={`${BUCKET_URL}/banners/desktop_img3.png`}
     />
     <source
       media="(max-width: 1024px)"
@@ -57,7 +57,7 @@ export const bannerImages = [
 
     <img
       src={`${BUCKET_URL}/banners/desktop_img3.png`}
-      className="w-full object-contain md:object-cover rounded-lg"
+      className="w-[40rem] object-contain md:object-cover rounded-lg"
       alt={"banner"}
     />
   </picture>,
