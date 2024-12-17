36833efd75474eeebfdda9e039f9612c4c8449c5
Celio <celio1878@hotmail.com>
2024-12-16 22:47:12 -0300

    fix: permissions in my book page

--- src/app/profile/[name]/books/[id]/page.tsx
+++ src/app/profile/[name]/books/[id]/page.tsx
@@ -7,17 +7,28 @@ import { MyBookChapters } from "@/components/book/my-book-chapters";
 import useSWR from "swr";
 import { fetcher } from "@/hooks/fetcher";
 import { BookDto } from "@/app/model/book-dto";
-import { useParams } from "next/navigation";
+import { redirect, useParams } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 const SERVICE_URL = String(process.env.NEXT_PUBLIC_BOOKS_API_URL);
 
 export default function MyBookPage() {
-  const { id } = useParams();
+  const { id, name } = useParams();
+  const { data: session } = useSession() as any;
+
+  const normalized_name = session?.user?.name
+    ?.trim()
+    .toLowerCase()
+    .replaceAll(" ", "-");
 
   const { data: book, isLoading } = useSWR(
     `${SERVICE_URL}/${id}`,
     fetcher<BookDto>({}).get,
   );
+
+  if (normalized_name !== name) {
+    return redirect(`https://www.beyourstories.com.br/books/${id}`);
+  }
 
   if (isLoading) return <Loading />;
 
